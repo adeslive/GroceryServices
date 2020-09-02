e0caa6fc146dfc08bb5c99aa4a3ba97e66d81db2
Adeslive <adeslive@outlook.es>
2020-09-01 18:34:42 -0600

    first commit

---
+++ commons.js
@@ -0,0 +1,37 @@
+const STRINGS = {
+    TITLE: 'Grocery Services',
+    SCREENS: {
+        HOME: 'Grocery Services',
+        ABOUT: "About",
+        EXIT: "Exit"
+    },
+}
+
+const URLS = {
+    ROOT:"https://webstore.ftssol.com/",
+    HOMEPAGE: "https://webstore.ftssol.com/CygnusWebStore_GroceryServices/",
+    ABOUT_US: "https://webstore.ftssol.com/CygnusWebStore_GroceryServices/content/4-about-us"
+}
+
+const COLORS = {
+    PRIMARY_COLOR: "#326949",
+}
+
+const IMAGES = {
+    FLIP_IMAGE: require('./assets/flip.png'),
+    NO_INTERNET: require('./assets/nointernet.png'),
+    MENU: {
+        HAMBURGUER: require('./assets/md-menu.png'),
+        ARROWS: {
+            LEFT: require('./assets/md-left-arrow.png'),
+            RIGHT: ''
+        }
+    }
+}
+
+export {
+    STRINGS,
+    URLS,
+    COLORS,
+    IMAGES
+}
